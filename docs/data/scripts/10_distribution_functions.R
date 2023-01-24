19c36c3af23a0238522bc7f65709e9d0609e4947
Rachel Warnock <rachel.warnock@gmail.com>
2023-01-24 11:29:35 +0100

    minor

--- docs/data/scripts/10_distribution_functions.R
+++ docs/data/scripts/10_distribution_functions.R
@@ -1,6 +1,16 @@
 
+
+dunif(1, -2, 2)
+dunif(1.5, -2, 2)
+
+curve( dunif(x, -2, 2), from = -4, to = 4 )
+
+
+dnorm(2, mean = 0, sd = 1)
+dnorm(0.2, mean = 0, sd = 1)
+
 # density
-curve( dnorm(1, mean = 0, sd = 1), from = -4, to = 4 )
+curve( dnorm(x, mean = 0, sd = 1), from = -4, to = 4 )
 
 # make it a bit fancier
 curve( dnorm(x, mean = 0, sd = 1), from = -4, to = 4, bty="n", 
@@ -9,16 +19,21 @@ abline(v = 0, lwd = 2, col = "grey", lty = 2)
 
 # cumulative distribution function (% of values < than)
 pnorm(0, mean = 0, sd = 1)
+pnorm(1, mean = 0, sd = 1)
 
 curve( pnorm(x, mean = 0, sd = 1), from = -4, to = 4, bty="n", 
        lwd = 3, col = "#84CA72", ylab = "Cumulative probability")
 abline(h = 0.5, lwd = 2, col = "grey", lty = 2)
 
 # quantile function (inverse of cumulative distribution)
-qnorm(0.5, mean = 0, sd = 1) 
+
+pnorm(0, mean = 0, sd = 1)
+qnorm(0.5, mean = 0, sd = 1)
+
+qnorm(0.8413447, mean = 0, sd = 1) 
 
 # generate random numbers
-n = 10
+n = 10000
 rnorm(n, mean = 0, sd = 1)
 rn = rnorm(n, mean = 0, sd = 1)
 
